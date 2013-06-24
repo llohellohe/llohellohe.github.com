f1f604c15bb618dd89a9b5c88d3b9b08bbc04175
llohellohe <yangqichris@gmail.com>
2013-06-24 21:44:45 +0800

    add post

--- special/custom.js
+++ special/custom.js
@@ -3,16 +3,19 @@ $("#o").click(function(){
 	var img=$("#o").attr('src');
 
 	if(img=="loveones.jpg"){
-		$("#o").attr('src','w.jpeg');
+		$("#o").attr('src','w.jpg');
 	}
 
-	if(img=="w.jpeg"){
-		$("#o").attr('src','m.jpeg');
+	if(img=="w.jpg"){
+		$("#o").attr('src','m.jpg');
 	}
 
-	if(img=="m.jpeg"){
+	if(img=="m.jpg"){
 		$("#o").attr('src','loveones.jpg');
+		$("#address").text("杨期和钱科佳诚挚邀请您参加在背景时间2013年10月6日晚上5点，在宁波市北仑区小港街道新政村杨家137号举行的婚礼晚宴!");
 	}
+	
+	$("#address").show('SLOW');
 });
 
 
